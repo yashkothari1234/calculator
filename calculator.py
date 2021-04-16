5b7fc63c0fc8253137d2e47373334ed1fe59e1b7
yashkothari1234 <68227727+yashkothari1234@users.noreply.github.com>
2021-04-16 12:01:43 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,19 @@
+x = int(input("Enter a number"))
+y = int(input("Enter the second number"))
+operation = input("Enter operation")
+
+if(operation=="addition"):
+    print(x+y)
+    
+if(operation=="subtraction"):
+    print(x-y)
+
+if(operation=="multiplication"):
+    print(x*y)
+
+if(operation=="division"):
+    print(x/y)
+
+
+
+    
